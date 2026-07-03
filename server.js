9069c00700ce019b13d903c02468922445714929
GitHubfukuyama24fi <GitHub24fi098@ms.dendai.ac.jp>
2026-07-03 10:45:45 +0900

    severクラス作りました～

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const http = require('http')
+//変数じゃなくてライブラリの機能。{}は消さないで
+const { Server } = require('socket.io')
+const cors = require('cors');
+
+const app = express();
+//corsは別のドメインからの勝手なアクセスをブロックする
+//アクセスsの許可証を発行
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    //originは通信のチェック
+    // プロトコル(http, https),ホスト名(localhost, google.comなど), ポート番号(3000,8080など)のうち一つでも違うと外部とみなされる
+    cors:{
+        origin: "*", //後で更新。どの場所から来たアクセスでも全部許可する
+        methods: ["GET", "POST"]
+    }
+});
+
+//キャンバスの状態を保持する変数
+let canvasState = [];
+
+//接続時の処理
+io.on('connection', (socket) => {
+    console.log('ユーザーが接続しました：'+socket.id);
+
+    //初回接続時に現在の状態を送信
+    socket.emit('init-state', canvasState);
+
+    //要素が追加されたときの処理
+    socket.on('update-canvas', (data) => {
+        canvasState = data;
+
+        //全員に最新状態を共有
+        socket.broadcast.emit('state-changed', canvasState);
+    });
+});
+
+//サーバ起動
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log('サーバーがポート ${PORT} で起動しました');
+});
