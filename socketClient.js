318850e18979d7953ceadfa6c4b8d1c8b525a207
GitHubfukuyama24fi <GitHub24fi098@ms.dendai.ac.jp>
2026-07-03 11:23:37 +0900

    接続テスト用のファイル送ります

---
+++ socketClient.js
@@ -0,0 +1,21 @@
+import { io } from "socket.io-client";
+
+//サーバのURL
+const SERVER_URL = "http://localhost:3000";
+
+//ソケットのインスタンスを作成
+const socket = io(SERVER_URL, {
+    reconnectionAttempts: 5, //接続エラーの再試行回数
+});
+
+//デバッグ。接続確認用のログ
+socket.on("connect", () => {
+  console.log("バックエンドサーバーと接続しました！ ID:", socket.id);
+});
+
+//接続エラー時
+socket.on("connect_error", (err) => {
+    console.error("サーバー接続エラー：", err.message);
+});
+
+export default socket;
